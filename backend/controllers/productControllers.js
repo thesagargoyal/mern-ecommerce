40e9110857d95885354a441d9e7233cb42cfe0b1
Sagar Goyal <sagargoyal2015@gmail.com>
2021-07-08 04:34:41 -0700

    Added avanced filters

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,26 +1,32 @@
 const Product = require('../models/product');
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const APIFeatures = require('../utils/apiFeatures');
 
 // adding new product admin/api/v1/product/new
 exports.newProduct = catchAsyncErrors( async (req, res, next)=>{
 
-
-        const product = await Product.create(req.body);
-        res.status(200).json({
-            success: true,
-            product
-        })
-
+    const product = await Product.create(req.body);
+    res.status(200).json({
+        success: true,
+        product
+    })
 
 })
 
 // get all products /api/v1/products
 exports.getProducts = catchAsyncErrors(async (req, res, next)=>{
     
+    const resPerPage = 4;
+    const productCount = await Product.countDocuments();
 
-        const products = await Product.find();
-        res.status(200).json({success: true, count: products.length, products });
+    const apiFeatures = new APIFeatures(Product.find(), req.query)
+        .search()  
+        .filter()
+        .pagination(resPerPage)
+    
+    let products = await apiFeatures.query;
+    res.status(200).json({success: true, count: products.length, productCount, products });
 
 
 })
