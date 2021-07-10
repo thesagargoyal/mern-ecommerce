17a8b9c378a732ce8f9266b3e820d9d343935d21
Sagar Goyal <sagargoyal2015@gmail.com>
2021-07-11 00:53:57 +0530

    Order backend

---
+++ backend/routes/order.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const {newOrder, getSingleOrder, myOrders, allOrders}=require('../controllers/orderController');
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/auth');
+
+router.post('/order/new', isAuthenticatedUser, newOrder);
+router.get('/order/:id', isAuthenticatedUser, getSingleOrder);
+router.get('/orders/me', isAuthenticatedUser, myOrders);
+router.get('/admin/orders', isAuthenticatedUser, authorizeRoles('admin'), allOrders);
+module.exports=router;
