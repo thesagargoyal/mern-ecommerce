4f3163fcd61a9fa3c96726165a6e49e5fb59f739
Sagar Goyal <sagargoyal2015@gmail.com>
2021-07-20 19:06:57 +0530

    Order Summary

--- frontend/src/components/cart/Cart.js
+++ frontend/src/components/cart/Cart.js
@@ -5,12 +5,34 @@ import alert from "react-alert";
 
 import { useDispatch, useSelector } from "react-redux";
 import { getProductDetails, clearErrors } from "../../actions/productActions";
-import { addItemToCart } from "../../actions/cartActions";
+import { addItemToCart, removeItemFromCart } from "../../actions/cartActions";
 
 const Cart = () => {
   const dispatch = useDispatch();
 
   const { cartItems } = useSelector((state) => state.cart);
+
+  const increaseQty = (id, quantity, stock) => {
+    const newQty = quantity + 1;
+
+    if (newQty > stock) return;
+
+    dispatch(addItemToCart(id, newQty))
+}
+
+const decreaseQty = (id, quantity) => {
+
+    const newQty = quantity - 1;
+
+    if (newQty <= 0) return;
+
+    dispatch(addItemToCart(id, newQty))
+
+}
+
+  const removeCartItemHandler = (id) => {
+    dispatch(removeItemFromCart(id));
+  }
 
   return (
     <>
@@ -28,7 +50,7 @@ const Cart = () => {
               {cartItems.map((item) => (
                 <>
                   <hr />
-                  <div class="cart-item">
+                  <div class="cart-item" key={item.product}>
                     <div class="row">
                       <div class="col-4 col-lg-3">
                         <img
@@ -49,22 +71,21 @@ const Cart = () => {
 
                       <div class="col-4 col-lg-3 mt-4 mt-lg-0">
                         <div class="stockCounter d-inline">
-                          <span class="btn btn-danger minus">-</span>
+                        <span className="btn btn-danger minus" onClick={() => decreaseQty(item.product, item.quantity)}>-</span>
                           <input
                             type="number"
                             class="form-control count d-inline"
-                            value="1"
+                            value={item.quantity}
                             readOnly
                           />
-
-                          <span class="btn btn-primary plus">+</span>
+                        <span className="btn btn-primary plus" onClick={() => increaseQty(item.product, item.quantity, item.stock)}>+</span>
                         </div>
                       </div>
 
                       <div  class="col-4 col-lg-1 mt-4 mt-lg-0">
                         <i
                           id="delete_cart_item"
-                          class="fa fa-trash btn btn-danger"
+                          class="fa fa-trash btn btn-danger" onClick={() => removeCartItemHandler(item.product)}
                         ></i>
                       </div>
                     </div>
@@ -79,10 +100,10 @@ const Cart = () => {
                 <h4>Order Summary</h4>
                 <hr />
                 <p>
-                  Subtotal: <span class="order-summary-values">3 (Units)</span>
+                  Subtotal: <span class="order-summary-values">{cartItems.reduce((acc, item)=> (acc + Number(item.quantity)), 0)} (Units)</span>
                 </p>
                 <p>
-                  Est. total: <span class="order-summary-values">$765.56</span>
+                  Est. total: <span class="order-summary-values">${cartItems.reduce((acc, item)=> (acc + item.quantity*item.price), 0).toFixed(2)}</span>
                 </p>
 
                 <hr />
