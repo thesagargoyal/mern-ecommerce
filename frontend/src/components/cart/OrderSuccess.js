77cc2fda2ca5db3574f8e1cd3e9e393494c7b74c
Sagar Goyal <sagargoyal2015@gmail.com>
2021-12-10 02:43:24 +0530

    Added Payment

--- frontend/src/components/cart/OrderSuccess.js
+++ frontend/src/components/cart/OrderSuccess.js
@@ -1,29 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import MetaData from "../layouts/MetaData";
+import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom'
+import MetaData from '../layouts/MetaData'
 
 const OrderSuccess = () => {
-  return (
-    <>
-      <MetaData title={"Order Success"} />
+    return (
+        <Fragment>
 
-      <div className="row justify-content-center">
-        <div className="col-6 mt-5 text-center">
-          <img
-            className="my-5 img-fluid d-block mx-auto"
-            src="/images/order_success.png"
-            alt="Order Success"
-            width="200"
-            height="200"
-          />
+            <MetaData title={'Order Success'} />
 
-          <h2>Your Order has been placed successfully.</h2>
+            <div className="row justify-content-center">
+                <div className="col-6 mt-5 text-center">
+                    <img className="my-5 img-fluid d-block mx-auto" src="/images/order_success.png" alt="Order Success" width="200" height="200" />
 
-          <Link to="/orders/me">Go to Orders</Link>
-        </div>
-      </div>
-    </>
-  );
-};
+                    <h2>Your Order has been placed successfully.</h2>
 
-export default OrderSuccess;
+                    <Link to="/orders/me">Go to Orders</Link>
+                </div>
+
+            </div>
+
+        </Fragment>
+    )
+}
+
+export default OrderSuccess
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -77,32 +77,36 @@ export const myOrdersReducer = (state = { orders:[]}, action)=>{
     }
 }
 
-export const orderDetailsReducer = (state = { orders:{}}, action)=>{
-  switch (action.type) {
+export const orderDetailsReducer = (state = { order: {} }, action) => {
+    switch (action.type) {
 
-      case ORDER_DETAILS_REQUEST:
-          return {
-              loading: true,
-          }
-      case ORDER_DETAILS_SUCCESS:
-          return{
-              loading: false,
-              orders: action.payload,
-          }
-      case ORDER_DETAILS_FAIL:
-          return{
-              loading: false,
-              error: action.payload,
-          }
-      case CLEAR_ERRORS:
-          return{
-              ...state,
-              error: null,
-          }
-      default:
-          return state;
-  }
+        case ORDER_DETAILS_REQUEST:
+            return {
+                loading: true
+            }
+
+        case ORDER_DETAILS_SUCCESS:
+            return {
+                loading: false,
+                order: action.payload
+            }
+
+        case ORDER_DETAILS_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+        case CLEAR_ERRORS:
+            return {
+                ...state,
+                error: null
+            }
+
+        default:
+            return state;
+    }
 }
+
 
 export const allOrdersReducer = (state = { orders: [] }, action) => {
   switch (action.type) {
